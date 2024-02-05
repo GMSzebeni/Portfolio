806170ef5f7ef7ece38d4c0816c4c6a5bf7bf1bd
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2024-02-05 16:00:19 +0100

    trying out with credentials

--- gmszm-portfolio/src/services/MessageService.tsx
+++ gmszm-portfolio/src/services/MessageService.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class MessageService {
     sendMessage(message: { name: string; email: string; subject: string; messageText: string; honeypot: string; }){
-        return axios.post('https://www.gmszm.hu/send-message', message);
+        return axios.post('https://www.gmszm.hu/send-message', message, {withCredentials: true});
     }
 }
 export default new MessageService();
