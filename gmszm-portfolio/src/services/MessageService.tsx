2e0fc4fc9a0aab65fb6d041d137a15f5f6c7a857
Gabriella Szebeni-Molnár <gabriella.szebeni@gmail.com>
2024-02-06 15:10:32 +0100

    update axios url

--- gmszm-portfolio/src/services/MessageService.tsx
+++ gmszm-portfolio/src/services/MessageService.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 class MessageService {
     sendMessage(message: { name: string; email: string; subject: string; messageText: string; honeypot: string; }){
-        return axios.post('https://www.gmszm.hu/send-message', message, {withCredentials: true});
+        return axios.post('https://gmszm-portfolio-mail.onrender.com/send-message', message, {withCredentials: true});
     }
 }
 export default new MessageService();
